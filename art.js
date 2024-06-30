abceb591242e930975a384da482326890a144bd9
RickInventions <155022228+RickInventions@users.noreply.github.com>
2024-06-30 10:49:18 +0100

    Add files via upload

---
+++ art.js
@@ -0,0 +1,10 @@
+function toggleContent(id){
+    var content = document.getElementById(id);
+    if (content.style.display === "none") {
+        content.style.display = "block";
+
+
+    }else{
+        content.style.display = "none";
+    }
+}
